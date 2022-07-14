01d5dc3a9f2addd0aedd6eb96fb551a512c73153
Surya2742 <boby7724284@gmail.com>
2022-07-15 01:33:44 +0530

    UC4_Day_3

---
+++ UC4.java
@@ -0,0 +1,34 @@
+
+public class UC4 {
+
+	public static void main(String[] args) {
+	int Full_Time_Working = 1;
+	int Full_Time_Hour = 8;
+	int IS_PART_TIME = 2;
+	int Part_Time_Hour = 4;
+	int Wage_per_Hour = 20;
+	int Emp_Check = (int) Math.floor(Math.random() * 3 );
+	
+	switch (Emp_Check) {
+	
+	case 1:
+	{
+		System.out.println("Employee is Present");
+		System.out.println("Working time is " + Full_Time_Hour + " and the salary is " + Full_Time_Hour * Wage_per_Hour);
+		break;
+		}
+	case 2:
+	{
+			System.out.println("Employee is Working Part Time");
+			System.out.println("Working time is " + Part_Time_Hour + " and the salary is " + Part_Time_Hour * Wage_per_Hour);
+			break;
+		}
+		
+	default:
+		System.out.println("Employee is Absent");
+		break;
+		
+	}
+	
+	}
+}
