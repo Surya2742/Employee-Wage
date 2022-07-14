9de7af2e78d28550a1c146771b5687352283c2f2
Surya2742 <boby7724284@gmail.com>
2022-07-14 18:21:17 +0530

    UC1_Day_3

---
+++ UC1.java
@@ -0,0 +1,14 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+	int Full_Time_Working = 1;
+	double Emp_Check = Math.floor(Math.random() * 2 );
+	if (Emp_Check == Full_Time_Working)
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is Absent");
+
+	}
+
+}
