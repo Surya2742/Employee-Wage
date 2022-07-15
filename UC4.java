0379be2b7ea29988554e1e41775edca047ec763d
Surya2742 <boby7724284@gmail.com>
2022-07-15 10:14:04 +0530

    UC4_Day_3

--- UC4.java
+++ UC4.java
@@ -15,13 +15,14 @@ public class UC4 {
 		System.out.println("Employee is Present");
 		System.out.println("Working time is " + Full_Time_Hour + " and the salary is " + Full_Time_Hour * Wage_per_Hour);
 		break;
-		}
+	}
+	
 	case 2:
 	{
 			System.out.println("Employee is Working Part Time");
 			System.out.println("Working time is " + Part_Time_Hour + " and the salary is " + Part_Time_Hour * Wage_per_Hour);
 			break;
-		}
+	}
 		
 	default:
 		System.out.println("Employee is Absent");
